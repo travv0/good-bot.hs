f566951697924c17a36f604f590c743014ba08a3
Travis <tssund93@gmail.com>
2021-01-11 00:33:14 -0600

    fallback to urban dictionary when definition not found in merriam

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,19 +4,22 @@
 
 module Lib where
 
-import Control.Lens ((^.))
-import Control.Monad (void, when)
+import Control.Applicative (Alternative (empty))
+import Control.Lens ((&), (.~), (^.))
+import Control.Monad (when)
 import Control.Monad.Reader (MonadIO (..), MonadReader (ask), ReaderT (runReaderT), forM_)
-import Data.Aeson (FromJSON, decode)
-import Data.ByteString.Lazy (ByteString)
-import Data.Text (Text)
+import Data.Aeson (FromJSON (parseJSON), Value (Object), eitherDecode, (.:))
+import qualified Data.ByteString.Lazy as BSL
+import Data.Text (Text, chunksOf)
 import qualified Data.Text as T
+import Data.Text.Encoding (encodeUtf8)
 import qualified Data.Text.IO as T
 import qualified Discord as D
 import qualified Discord.Requests as D
 import qualified Discord.Types as D
 import GHC.Generics (Generic)
-import Network.Wreq (Response, get, responseBody)
+import Network.Wreq (Response, defaults, get, getWith, header, param, responseBody)
+import qualified Network.Wreq as W
 import System.Environment (getEnv)
 import System.Random (Random (randomIO))
 
@@ -49,7 +52,12 @@ typingStart (D.TypingInfo userId channelId _utcTime) = do
 createMessage :: (MonadReader D.DiscordHandle m, MonadIO m) => D.ChannelId -> Text -> m ()
 createMessage channelId message = do
     dis <- ask
-    liftIO $ void $ D.restCall dis $ D.CreateMessage channelId message
+    let chunks = chunksOf 2000 message
+    forM_ chunks $ \chunk -> do
+        r <- liftIO $ D.restCall dis $ D.CreateMessage channelId chunk
+        case r of
+            Right _ -> return ()
+            Left err -> liftIO $ print err
 
 createGuildBan ::
     (MonadReader D.DiscordHandle m, MonadIO m) =>
@@ -59,13 +67,16 @@ createGuildBan ::
     m ()
 createGuildBan guildId userId banMessage = do
     dis <- ask
-    liftIO $
-        void $
+    r <-
+        liftIO $
             D.restCall dis $
                 D.CreateGuildBan
                     guildId
                     userId
                     (D.CreateGuildBanOpts Nothing (Just banMessage))
+    case r of
+        Right _ -> return ()
+        Left err -> liftIO $ print err
 
 fromBot :: D.Message -> Bool
 fromBot m = D.userIsBot (D.messageAuthor m)
@@ -87,17 +98,29 @@ isRussianRoulette = ("!rr" `T.isPrefixOf`) . T.toLower
 dictionaryKey :: IO Text
 dictionaryKey = T.pack <$> getEnv "BIGBOT_DICT_KEY"
 
-data Definition = Definition {fl :: Text, shortdef :: [Text]}
+urbanKey :: IO Text
+urbanKey = T.pack <$> getEnv "BIGBOT_URBAN_KEY"
+
+data Definition = Definition
+    { defPartOfSpeech :: Maybe Text
+    , defDefinitions :: [Text]
+    }
     deriving (Generic, Show)
 
-instance FromJSON Definition
+instance FromJSON Definition where
+    parseJSON (Object v) = do
+        partOfSpeech <- v .: "fl"
+        definitions <- v .: "shortdef"
+        return Definition{defPartOfSpeech = partOfSpeech, defDefinitions = definitions}
+    parseJSON _ = empty
 
 define :: (MonadIO m, MonadReader D.DiscordHandle m) => D.Message -> m ()
 define message = do
     apiKey <- liftIO dictionaryKey
+    urbanApiKey <- liftIO urbanKey
     let (_ : wordsToDefine) = words $ T.unpack $ D.messageText message
     forM_ wordsToDefine $ \word -> do
-        moutput <- getOutput apiKey word
+        moutput <- getOutput apiKey urbanApiKey word
         case moutput of
             Just output -> createMessage (D.messageChannel message) output
             Nothing ->
@@ -109,8 +132,8 @@ isDefine = ("!define " `T.isPrefixOf`) . T.toLower
 
 buildOutput :: String -> Definition -> Text
 buildOutput word definition = do
-    let shortDefinition = shortdef definition
-        partOfSpeech = fl definition
+    let shortDefinition = defDefinitions definition
+        mpartOfSpeech = defPartOfSpeech definition
         definitions = case shortDefinition of
             [def] -> def
             defs ->
@@ -120,17 +143,30 @@ buildOutput word definition = do
                         [1 :: Int ..]
                         defs
         formattedOutput =
-            "**" <> T.pack word <> "** *" <> partOfSpeech <> "*\n" <> definitions
+            "**" <> T.pack word <> "**"
+                <> ( case mpartOfSpeech of
+                        Just partOfSpeech -> " *" <> partOfSpeech <> "*"
+                        Nothing -> ""
+                   )
+                <> "\n"
+                <> definitions
      in formattedOutput
 
-getOutput :: MonadIO m => Text -> String -> m (Maybe Text)
-getOutput apiKey word = do
+getOutput :: MonadIO m => Text -> Text -> String -> m (Maybe Text)
+getOutput apiKey urbanApiKey word = do
     response <- getDictionaryResponse apiKey word
-    case decode (response ^. responseBody) of
-        Just defs -> return $ Just $ T.intercalate "\n\n" $ map (buildOutput word) defs
-        Nothing -> return Nothing
+    urbanResponse <- getUrbanResponse urbanApiKey word
+    case eitherDecode (response ^. responseBody) <> decodeUrban (urbanResponse ^. responseBody) of
+        Right defs
+            | not (null defs) ->
+                return $
+                    Just $
+                        T.intercalate "\n\n" $
+                            map (buildOutput word) defs
+        Left err -> liftIO (print err) >> return Nothing
+        _ -> return Nothing
 
-getDictionaryResponse :: MonadIO m => Text -> String -> m (Response ByteString)
+getDictionaryResponse :: MonadIO m => Text -> String -> m (Response BSL.ByteString)
 getDictionaryResponse apiKey word =
     liftIO $
         get $
@@ -139,3 +175,35 @@ getDictionaryResponse apiKey word =
                     <> T.pack word
                     <> "?key="
                     <> apiKey
+
+getUrbanResponse :: MonadIO m => Text -> String -> m (Response BSL.ByteString)
+getUrbanResponse apiKey word =
+    liftIO $
+        getWith
+            (urbanOpts apiKey word)
+            "https://mashape-community-urban-dictionary.p.rapidapi.com/define"
+
+urbanOpts :: Text -> String -> W.Options
+urbanOpts apiKey term =
+    defaults
+        & header "x-rapidapi-key" .~ [encodeUtf8 apiKey]
+        & header "x-rapidapi-host" .~ ["mashape-community-urban-dictionary.p.rapidapi.com"]
+        & header "useQueryString" .~ ["true"]
+        & param "term" .~ [T.pack term]
+
+newtype UrbanDefinition = UrbanDefinition {urbanDefDefinition :: [Text]}
+    deriving (Generic, Show)
+
+instance FromJSON UrbanDefinition where
+    parseJSON (Object v) = do
+        list <- v .: "list"
+        defs <- mapM (.: "definition") list
+        return UrbanDefinition{urbanDefDefinition = defs}
+    parseJSON _ = empty
+
+decodeUrban :: BSL.ByteString -> Either String [Definition]
+decodeUrban = fmap urbanToDictionary . eitherDecode
+
+urbanToDictionary :: UrbanDefinition -> [Definition]
+urbanToDictionary (UrbanDefinition def) =
+    [Definition Nothing def | not (null def)]
